2aecc73cf9cbab80e86260a25ff1b53496dc425d
Saqib Ashraf <linktosaqib@gmail.com>
2025-07-14 22:41:17 +0500

    Universal Import Export for Opencart

---
+++ upload/admin/language/en-gb/extension/module/product_export.php
@@ -0,0 +1,49 @@
+<?php
+// Heading
+$_['heading_title']    = 'Product Export';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified product export module!';
+$_['text_edit']        = 'Edit Product Export Module';
+$_['text_export']      = 'Export';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Button
+$_['button_export_descriptions'] = 'Export Product Descriptions';
+$_['button_export_options']      = 'Export Product Options';
+$_['button_export_attributes']   = 'Export Product Attributes';
+$_['button_export_images']       = 'Export Product Images';
+$_['button_export_data']         = 'Export Product Data';
+$_['button_export_p_to_cat']         = 'Export Product To Category';
+$_['button_export_p_to_store']         = 'Export Product To Store';
+$_['button_export_p_related']         = 'Export Product Related';
+$_['button_export_p_specials']         = 'Export Product Specials';
+$_['button_export_p_discounts']         = 'Export Product Discounts';
+$_['button_export_p_rewards']         = 'Export Product Rewards';
+$_['button_export_option_values']         = 'Export Product Option Values';
+
+// Customer Data
+$_['text_customer_data']         = 'Customer Data';
+$_['button_export_customers']         = 'Export Customers';
+$_['button_export_customer_groups']   = 'Export Customer Groups';
+$_['button_export_customer_addresses'] = 'Export Customer Addresses';
+$_['button_export_customer_affiliates'] = 'Export Customer Affiliates';
+$_['button_export_customer_transactions'] = 'Export Customer Transactions';
+$_['button_export_customer_rewards']    = 'Export Customer Rewards';
+$_['button_export_customer_ips']        = 'Export Customer IPs';
+
+// Order Data
+$_['text_order_data']         = 'Order Data';
+$_['button_export_orders']         = 'Export Orders';
+$_['button_export_order_products']   = 'Export Order Products';
+$_['button_export_order_options'] = 'Export Order Options';
+$_['button_export_order_totals'] = 'Export Order Totals';
+$_['button_export_order_histories']    = 'Export Order Histories';
+
+
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify product export module!';
